169e7b27752732ddeb81a0d265886ce44fb66b43
Vladimir-Aires <vlad.aires05@gmail.com>
2024-09-18 00:17:27 -0300

    Criação da primeira página - Início

--- Atividades/atividade05/projeto_serietv/src/app/api/apiSeries.js
+++ Atividades/atividade05/projeto_serietv/src/app/api/apiSeries.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const apiSeries = axios.create({
     baseURL: "https://api.themoviedb.org/3",
     headers: {
-        Authorization: 'Bearer' + process.env.NEXT_PUBLIC_TMDB_API_KEY
-    }
-})
+        Authorization: "Bearer " + process.env.NEXT_PUBLIC_TMDB_API_KEY,
+    },
+});
 
-export default apiSeries
+export default apiSeries;
--- Atividades/atividade05/projeto_serietv/src/app/series/page.js
+++ Atividades/atividade05/projeto_serietv/src/app/series/page.js
@@ -1,12 +1,65 @@
+'use client'
+
+import { useEffect, useState } from "react";
 import EstruturaPagina from "../components/EstruturaPagina";
+import apiSeries from "../api/apiSeries";
+import { Card, CardBody, CardImg, Col, Row } from "react-bootstrap";
 
 
 export default function page() {
-  return (
-    <>
-        <EstruturaPagina titulo='Séries Populares'>
 
+    const [series, setSeries] = useState([])
+
+    useEffect(() => {
+        buscaSeries();
+    }, []);
+
+    async function buscaSeries(){
+        const consulta = await apiSeries.get("/tv/popular?language=pt-BR")
+        console.log(consulta.data.results);
+
+        const seriesRecebidas = consulta.data.results;
+
+        setSeries(seriesRecebidas)
+    }
+
+  return (
+    
+        <EstruturaPagina titulo='Séries Populares'>
+          <Row md={3} sm={2}>
+            {series.map((serie) => {
+              return(
+                <Col className="py-3">
+                  <Card style={{height: '100%'}}>
+                   <Card.Img src={"https://image.tmdb.org/t/p/w500/" + 
+serie.poster_path}/>
+
+                   <Card.Body>
+                    <Card.Title>
+                      {serie.name}
+                    </Card.Title>
+                    <Card.Text>
+                      <p>
+                        Nota:{} <br />
+                        Lançamento:{}
+                      </p>
+                      
+                    </Card.Text>
+                      
+                    
+                   </Card.Body>
+                   <Card.Footer>
+
+                   </Card.Footer>
+                  </Card>
+                
+                
+                </Col>
+              )
+            })}
+          </Row>
+            
         </EstruturaPagina>
-    </>
+    
   )
 }
